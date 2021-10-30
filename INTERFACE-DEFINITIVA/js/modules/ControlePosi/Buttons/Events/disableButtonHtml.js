c401d032c218b02f6f7eb43ef1831b2281c3bd15
Pedro-Teodorio <79485773+Pedro-Teodorio@users.noreply.github.com>
2021-10-29 23:56:54 -0300

    Delete INTERFACE-DEFINITIVA directory

--- INTERFACE-DEFINITIVA/js/modules/ControlePosi/Buttons/Events/disableButtonHtml.js
+++
@@ -1,7 +0,0 @@
-const inputsHtml =  document.querySelector('.inputs')
-const btnAddPosicoes = document.getElementById("btn-add-posicao");
-
-function disableButtonHtml(){
-    btnAddPosicoes.addEventListener('click',()=>{inputsHtml.removeChild(btnAddPosicoes)})
-}
-export {disableButtonHtml}
--- INTERFACE-DEFINITIVA/js/modules/Historico/script.js
+++
@@ -1,5 +0,0 @@
-import {divLista} from "./Divs/DivLista.js";
-import { divPages } from "./Divs/DivPages.js";
-
-divLista()
-divPages()
